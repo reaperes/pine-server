3ce3c8a1b5fc98eaf2f23439629e7674255492ca
reaperes <emerald105@hanmail.net>
2014-10-21 13:05:58 +0900

    update 100% coverage

--- pine/models/user_model_test.py
+++ pine/models/user_model_test.py
@@ -4,6 +4,15 @@ from pine.models import UserModel
 
 
 class UserModelTest(TestCase):
+    def test_valid_user_model_should_be_success(self):
+        username = 'test@test.com'
+        user = UserModel.objects.create_user(username=username, password='password')
+        self.assertEqual(username, str(user))
+
+        # test required internal function
+        self.assertEqual(username, user.get_short_name())
+        self.assertEqual(username, user.get_full_name())
+
     def test_invalid_user_model_can_not_be_created(self):
         """Create invalid email should be raise ValidationError
         """
